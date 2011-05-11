56484c4d93c5067668137162f83b2008f597f89c
Brett Bieber <brett.bieber@gmail.com>
2011-05-11 18:22:24 -0500

    Prep for 0.2.0 release, will need to test first

--- extrasetup.php
+++ extrasetup.php
@@ -1,12 +1,12 @@
 <?php
 
-$pyrus = new \PEAR2\Pyrus\Package(__DIR__ . '/../../Pyrus/package.xml');
+$pyrus = new \PEAR2\Pyrus\Package(__DIR__ . '/../PEAR2_Pyrus/package.xml');
 $pyrus->setPackagingFilter('PEAR2\Pyrus\PackageFile\v2Iterator\MinimalPackageFilter');
 $extrafiles = array(
-    new \PEAR2\Pyrus\Package(__DIR__ . '/../../HTTP_Request/package.xml'),
+    new \PEAR2\Pyrus\Package(__DIR__ . '/../PEAR2_HTTP_Request/package.xml'),
     $pyrus,
-    new \PEAR2\Pyrus\Package(__DIR__ . '/../../Pyrus_Developer/package.xml'),
-    new \PEAR2\Pyrus\Package(__DIR__ . '/../../Exception/package.xml'),
-    new \PEAR2\Pyrus\Package(__DIR__ . '/../../MultiErrors/package.xml'),
+    new \PEAR2\Pyrus\Package(__DIR__ . '/../PEAR2_Pyrus_Developer/package.xml'),
+    new \PEAR2\Pyrus\Package(__DIR__ . '/../PEAR2_Exception/package.xml'),
+    new \PEAR2\Pyrus\Package(__DIR__ . '/../PEAR2_MultiErrors/package.xml'),
 );
 ?>
